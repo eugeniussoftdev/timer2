678ea346546bf4913e6f346be34ce63f70369ead
macbook <macbook@MacBook-Pro-mac.local>
2024-01-06 21:57:30 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    define: {
+      "process.env.API_KEY": JSON.stringify(env.API_KEY),
+      "process.env.AUTH_DOMAIN": JSON.stringify(env.AUTH_DOMAIN),
+      "process.env.PROJECT_ID": JSON.stringify(env.PROJECT_Id),
+      "process.env.STORAGE_BUCKET": JSON.stringify(env.STORAGE_BUCKET),
+      "process.env.MESSAGINE_SENDER_ID": JSON.stringify(
+        env.MESSAGINE_SENDER_ID
+      ),
+      "process.env.APP_ID": JSON.stringify(env.APP_ID),
+      "process.env.MEASUREMENT_ID": JSON.stringify(env.MEASUREMENT_ID),
+    },
+  };
+});
