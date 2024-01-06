2ef0c75e9eaed70b21e850b61fe44b507a9840ec
macbook <macbook@MacBook-Pro-mac.local>
2024-01-06 17:50:52 +0200

    timer component added

---
+++ src/components/Timer.tsx
@@ -0,0 +1,39 @@
+import React, { useRef, useState } from "react";
+
+import { formatTime } from "../helpers/formatTime";
+
+const Timer = () => {
+  const [time, setTimer] = useState(0);
+  const ref = useRef<number | undefined>(undefined);
+
+  const startTimer = () => {
+    console.log(ref.current);
+    if (ref) {
+      ref.current = setInterval(() => {
+        setTimer((prevState) => prevState + 1);
+        console.log(time);
+      }, 1000);
+    }
+  };
+
+  const pauseTimer = () => {
+    clearInterval(ref.current);
+  };
+
+  const stopTimer = () => {
+    setTimer(0);
+    clearInterval(ref.current);
+  };
+
+  return (
+    <div>
+      <div>{formatTime(time * 1000)}</div>
+
+      <button onClick={startTimer}>Start</button>
+      <button onClick={pauseTimer}>Pause</button>
+      <button onClick={stopTimer}>Stop</button>
+    </div>
+  );
+};
+
+export default Timer;
